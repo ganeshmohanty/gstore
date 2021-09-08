22e61e0ec2e321fe81eeaad4601914f6ed80e733
Ganesh <ganesh.mohanty.1999@gmail.com>
2021-09-08 19:04:20 +0530

    Inmplemented redux and created actions and reducers

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "react-redux";
 
 import App from "./App";
-
+import store from "./store";
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,18 +1,18 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Heading, Grid } from "@chakra-ui/react";
+
 import Product from "../components/Product";
-//import products from "../products";
+import { listProducts } from "../actions/productActions";
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, products, error } = productList;
+  console.log(products);
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
-    };
-    fetchProducts();
+    dispatch(listProducts());
   }, []);
   return (
     <>
